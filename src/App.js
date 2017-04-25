a729d15c65c87fd4269e798d5f6a29b75ba91d01
Patrick <patw.hi@gmail.com>
2017-04-25 09:56:17 +0800

    Initialize firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import Header from './components/Header/Header';
 
 class App extends Component {
+  componentWillMount() {
+    // Initialize Firebase
+    const config = {
+      apiKey: 'AIzaSyC8IIzKU2evHnf6_45X93orOe_1ui_Fbu8',
+      authDomain: 'rn-auth-practice.firebaseapp.com',
+      databaseURL: 'https://rn-auth-practice.firebaseio.com',
+      projectId: 'rn-auth-practice',
+      storageBucket: 'rn-auth-practice.appspot.com',
+      messagingSenderId: '130095734410',
+    };
+
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <View>
